fc25e45ee62ac1cc9e8e05f37a4f03d0bd5cbbc8
mehak <xyz@gmail.com>
2024-02-18 19:13:36 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <titile>hello function with click button event</titile>
+        <script type="javascript">
+            function msg(){
+                alert( "daffa ho jao");
+            }
+        </script>
+    </head>
+    <body>
+        <h2>click me</h2>
+        <form>
+        <input type="button" value="message from your loved one" onclick="msg()"/>
+            </form>
+    </body>
+</html>
